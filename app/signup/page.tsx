2d1568de9ec33167010251da3ecc9be0291026e8
Omoshola <heylehgbedey@gmail.com>
2025-11-05 15:09:49 -0800

    feat: implement signup form with validation using react-hook-form and zod

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,89 @@
+'use client'
+
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const SignUpSchema = z.object({
+    name: z.string("Name is required"),
+    email: z.email("Provide a valid email-address"),
+    password: z.string("Password is required"),
+});
+
+type SignupData = z.infer<typeof SignUpSchema>;
+
 function SignUpPage() {
+    const form = useForm<SignupData>({
+        resolver: zodResolver(SignUpSchema),
+    });
+
+    function onSubmit(data:SignupData) {
+        try{
+            const response = fetch('')
+        } catch {
+
+        }
+    }
+
     return (
-        <div>
-            
+        <div className="">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col max-w-sm mx-auto justify-center gap-3 h-screen">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Full Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Joe Doe" type="text" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Email" type="email" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Password" type="password" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
         </div>
-    )   
+    );
 }
 
 export default SignUpPage;
